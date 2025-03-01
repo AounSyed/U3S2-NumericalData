4b666e72c221454b8abf830981b919afd84fde7a
AounSyed <Aoungalaxy@gmail.com>
2025-03-01 09:28:49 -0500

    Completed assignment

--- src/main/java/com/codedifferently/lab/partA/Chickens01.java
+++ src/main/java/com/codedifferently/lab/partA/Chickens01.java
@@ -2,6 +2,31 @@ package com.codedifferently.lab.partA;
 
 public class Chickens01 {
     //Put your code here
+    public static void main(String[] args){
 
+        int eggsPerChicken = 5;
+        int chickenCount = 3;
+        int totalEggs = eggsPerChicken * chickenCount;
+
+        chickenCount++;
+        totalEggs = totalEggs + (chickenCount * eggsPerChicken);
+
+        chickenCount = chickenCount / 2;
+        totalEggs = totalEggs + (chickenCount * eggsPerChicken);
+
+        System.out.println(totalEggs);
+
+        int eggsPerChicken1 = 4;
+        int chickenCount1 = 8;
+        int totalEggs1 = eggsPerChicken1 * chickenCount1;
+
+        chickenCount1++;
+        totalEggs1 = totalEggs1 + (chickenCount1 * eggsPerChicken1);
+
+        chickenCount1 = chickenCount1 / 2;
+        totalEggs1 = totalEggs1 + (chickenCount1 * eggsPerChicken1);
+
+        System.out.println(totalEggs1);
+    }
     //System.out.println(totalEggs);
 }
--- src/main/java/com/codedifferently/lab/partA/Chickens02.java
+++ src/main/java/com/codedifferently/lab/partA/Chickens02.java
@@ -3,9 +3,16 @@ package com.codedifferently.lab.partA;
 public class Chickens02 {
     public static void main(String[] args) {
         //Put your code here
+        int eggs = 100;
+        eggs = eggs + 121;
+        eggs = eggs + 117;
 
-        //System.out.println("Daily Average:   " +dailyAverage);
-        //System.out.println("Monthly Average: " +monthlyAverage);
-        //System.out.println("Monthly Profit:  $" +monthlyProfit);
+        double dailyAverage = eggs / 3.0;
+        double monthlyAverage = dailyAverage * 30.0;
+        double monthlyProfit = monthlyAverage * 0.18;
+
+        System.out.println("Daily Average:   " +dailyAverage);
+        System.out.println("Monthly Average: " +monthlyAverage);
+        System.out.println("Monthly Profit:  $" +monthlyProfit);
     }
 }
